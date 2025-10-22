21fabccd24435632ce5ddf2f493a611a1e7e0030
준혁 <wnsgur3592@gmail.com>
2025-10-22 13:01:39 +0900

    스캐너 초안?

--- App.java
+++ App.java
@@ -3,6 +3,7 @@ import java.util.ArrayList;
 import myClass.Book;
 import myClass.User;
 import DataBase.LibDB;
+import java.util.Scanner;
 
 /**
  * App은 LibraryManagementSystem을 실행하고 이용자와 도서 정보를 불러와 대출과 현황을 확인하는 클래스이다
@@ -26,17 +27,23 @@ public class App {
         LibMS.printDB(bookDB);
         System.out.println();
         
-        // ArrayList<String> books = new ArrayList<String>();
+        Scanner scan = new Scanner(System.in);
+        for(int i=0; i<3; i++){
+            System.out.println("학생ID를 입력");
+            String tempStID = scan.nextLine();
+        
+            System.out.println("BookID를 입력");
+            String tempBookID = scan.nextLine();
+            LibMS.borrowBook(tempStID, tempBookID);
+        }
+        
+    
+        // ArrayList<String> books = new ArrayList<>();
         // books.add("B01");
         // books.add("B02");
         // LibMS.borrowBook("2025320001", books);
-
-        ArrayList<String> books = new ArrayList<>();
-        books.add("B01");
-        books.add("B02");
-        LibMS.borrowBook("2025320001", books);
-        LibMS.borrowBook("2024320002", "B03");
-        LibMS.borrowBook("2023320003", "B04");
+        // LibMS.borrowBook("2024320002", "B03");
+        // LibMS.borrowBook("2023320003", "B04");
 
         System.out.println("----- 대출 현황 -----");
         LibMS.printLoanList();
